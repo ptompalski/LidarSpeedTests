085620b4320ea4cce7ee856f714973fd3debec8c
Tompalski <piotr.tompalski@nrcan-rncan.gc.ca>
2024-04-17 15:17:10 -0700

    changes to the output format

--- cpu_ram_usage.ps1
+++ cpu_ram_usage.ps1
@@ -29,8 +29,18 @@ while($true) {
 	
     # $date + ' > CPU: ' + $cpuTime.ToString("#,0.000") + '%, Avail. Mem.: ' + $availMem.ToString("N0") + 'MB (' + (104857600 * $availMem / $totalRam).ToString("#,0.0") + '%)' | Export-CSV -Append -NoTypeInformation $Filepath
     # $all = $date + ' > CPU: ' + $cpuTime.ToString("#,0.000") + '%, Avail. Mem.: ' + $availMem.ToString("N0") + 'MB (' + (104857600 * $availMem / $totalRam).ToString("#,0.0") + '%) ' + $disk_read.ToString("#,0.000")+ ' ' + $disk_write.ToString("#,0.000")
-    $all = $date + ' > CPU: ' + $cpuTime.ToString("#,0.000") + '%, Avail. Mem.: ' + $availMem.ToString("N0") + 'MB (' + (104857600 * $availMem / $totalRam).ToString("#,0.0") + '%) ' 
-    $all | Out-File -Append $Filepath
+    
+	# $all = $date + ' > CPU: ' + $cpuTime.ToString("#,0.000") + '%, Avail. Mem.: ' + $availMem.ToString("N0") + 'MB (' + (104857600 * $availMem / $totalRam).ToString("#,0.0") + '%) ' 
+    
+	$all = "$date,$cpuTime,$availMem"
+	$all | Out-File -Append $Filepath
     $all 
 	Start-Sleep -s 2
 }
+
+
+    # Format the data as CSV
+    # $csvLine = "$timestamp,$cpuValue,$ramValue"
+
+    # Append the data to the CSV file
+    # $csvLine | Out-File -Append -FilePath $filePath
